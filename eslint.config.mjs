b2a1be4f1e60baec202dd9c7301bfd1a7b77edcb
soohyun4747 <soohyun4747@gmail.com>
2025-10-25 21:58:00 +0900

    eslint config updated

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,30 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  // ✅ Next.js + TypeScript 기본 설정
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-
-  // ✅ 커스텀 규칙 추가
-  {
-    rules: {
-      "@typescript-eslint/no-explicit-any": "off", // ← any 허용
-    },
-  },
-];
+const eslintConfig = defineConfig([
+	...nextVitals,
+	{
+		rules: {
+			'react/no-unescaped-entities': 'off',
+			'@next/next/no-page-custom-font': 'off',
+		},
+	},
+	// Override default ignores of eslint-config-next.
+	globalIgnores([
+		// Default ignores of eslint-config-next:
+		'.next/**',
+		'out/**',
+		'build/**',
+		'next-env.d.ts',
+	]),
+]);
 
 export default eslintConfig;
